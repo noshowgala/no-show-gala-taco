cf316564824295728bd5c6960780cc3befb01379
Nadia Odunayo <nodunayo@gmail.com>
2023-08-27 20:25:21 +0100

    Creates unstyled homepage

---
+++ spec/features/user_can_view_gala_details_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "User can view gala details" do
+  context "when visiting the homepage" do
+    it "shows information about the gala" do
+      visit root_path
+
+      expect(page).to have_content("the 6th Annual No Show Gala")
+      expect(page).to have_button("R.S.V.P.")
+    end
+  end
+end
