66f03842ef0c14345f4bcd95759cf13a592cd656
Nadia Odunayo <nodunayo@gmail.com>
2023-10-06 14:25:59 +0100

    Fix syntax error in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
   has_many :products_donations, class_name: "ProductDonation", dependent: :destroy
   has_many :donations, through: :products_donations
-end
 
   def image
     name.gsub(/\s+/, '_').downcase
