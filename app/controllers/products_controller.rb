c0b78652873baccd708dfcff10d24391743ea82a
Nadia Odunayo <nodunayo@gmail.com>
2023-10-08 06:07:44 +0100

    Make sure the browser's back button works by resetting the Products#index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :no_cache
   before_action :reset_donation, only: [:index]
 
   def index
@@ -9,5 +10,11 @@ class ProductsController < ApplicationController
 
   def reset_donation
     session.delete(:donation_id) if session[:donation_id]
+  end
+
+  def no_cache
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
