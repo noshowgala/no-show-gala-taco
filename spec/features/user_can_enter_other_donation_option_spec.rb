493edc7850a5f81c556fdff1fa1604563784a0af
sarony <saron.yitbarek@gmail.com>
2023-10-09 11:08:02 -0700

    Removes unnecessary js and setups spec though spec is failing

--- spec/features/user_can_enter_other_donation_option_spec.rb
+++ spec/features/user_can_enter_other_donation_option_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "User can enter 'other' donation amount" do
+RSpec.describe "User can enter 'other' donation amount", js: true, driver: :selenium_headless do
   it "shows the correct empty field" do
     other = Product.create(name: "Other", price: 0)
 
@@ -9,16 +9,27 @@ RSpec.describe "User can enter 'other' donation amount" do
     within "[data-product-id='#{other.id}']" do
       click_on "Enter amount"
       expect(page).to have_content("$")
-      expect(page).to have_content("Checkout (0)")
-
-      fill_in("product_price", with: "100")
-      click_on ("Add")
-      expect(page).to have_content("$100 Added")
-      expect(page).to have_content("Checkout (1)")
-
-      click_on("Added")
-      expect(page).not_to have_content("$100 Added")
-      expect(page).to have_content("Checkout (0)")
+      expect(page).not_to have_content("Enter amount")
     end
+
+    expect(page).to have_content("CHECKOUT (0)")
+
+    within "[data-product-id='#{other.id}']" do
+      fill_in("otherFinalPrice", with: 100)
+      find_button("Add").click
+      # click_on "Add"
+      save_and_open_screenshot
+      expect(page).to have_button("Added", wait: 10)
+      expect(page).to have_content("$100")
+    end
+
+    expect(page).to have_content("CHECKOUT (1)")
+ 
+    within "[data-product-id='#{other.id}']" do
+      click_on("Added")
+      expect(page).not_to have_content("$100")
+    end
+
+    expect(page).to have_content("CHECKOUT (0)")
   end
 end
