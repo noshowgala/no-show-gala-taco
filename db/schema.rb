854be6fcc9ea5a155c948e3f7384b84ba3d0161b
Nadia Odunayo <nodunayo@gmail.com>
2023-10-15 10:49:40 +0100

    Add migration for guest_list_entries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_09_045456) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_094810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_045456) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["stripe_transaction_id"], name: "index_donations_on_stripe_transaction_id"
+  end
+
+  create_table "guest_list_entries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
